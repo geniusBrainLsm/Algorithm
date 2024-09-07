3044d2a9565cdad943513c1678c301fad644aac0
geniusBrainLsm <87559232+geniusBrainLsm@users.noreply.github.com>
2024-09-07 17:30:59 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b) {
+        while(b%2==0 || b % 5 == 0){
+            if(b % 2 == 0){
+                b /= 2;
+            } else if(b % 5 == 0){
+                b /= 5;
+            }
+        }
+        return (a%b==0) ? 1 : 2;
+    }
+}
