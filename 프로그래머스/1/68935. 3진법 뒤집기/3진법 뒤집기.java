1781e6d58b8c50c67ce4e7bd24e344c240160894
geniusBrainLsm <87559232+geniusBrainLsm@users.noreply.github.com>
2024-09-16 18:00:42 +0900

    [level 1] Title: 3진법 뒤집기, Time: 13.67 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int n) {
         String str = "";
-
-        while (n != 0) {
+        while(n!=0){
             str += n % 3;
             n /= 3;
         }
-        
-        return Integer.parseInt(str, 3);
+        return Integer.parseInt(str,3);
     }
 }
