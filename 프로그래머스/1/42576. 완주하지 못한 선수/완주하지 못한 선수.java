4b0be896c44c5ddf31a450453cfaaf4dcae6b817
geniusBrainLsm <87559232+geniusBrainLsm@users.noreply.github.com>
2025-09-23 23:12:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.56 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,18 @@
-import java.util.LinkedHashMap;
-
-public class Solution {
-    public static String solution(String[] participant, String[] completion) {
-
-        LinkedHashMap<String, Integer> participantMap = new LinkedHashMap<>();
-        for(String name : participant){
-            participantMap.put(name, participantMap.getOrDefault(name, 0) + 1);
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String,Integer> map = new HashMap<>();
+    
+        for(int i = 0; i<participant.length; i++){
+            map.put(participant[i], map.getOrDefault(participant[i],0) + 1);
         }
-        
-        for(String name : completion) {
-            participantMap.put(name, participantMap.get(name) - 1);
+        for(int i = 0; i<completion.length; i++){
+            map.put(completion[i], map.get(completion[i])-1);
         }
-        
-        for (String name : participantMap.keySet()) {
-            if (participantMap.get(name) > 0) {
-                return name;
-            }
+        List<String> list = new ArrayList<>(map.keySet());
+        for(int i = 0; i < list.size(); i++){
+            if(map.get(list.get(i))>0) return list.get(i);
         }
-        return "";
+        return "false";
     }
 }
